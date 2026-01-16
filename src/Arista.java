c30c73ebc344647ddc1d70e3c5993d9cf4832b2e
odell1 <pablo.lledo@gmail.com>
2026-01-16 10:35:47 +0100

    first commit

---
+++ src/Arista.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Arista {
+    Nodo destino;
+    int tiempo; 
+
+    public Arista(Nodo destino, int tiempo) {
+        this.destino = destino;
+        this.tiempo = tiempo;
+    }
+}//Arista
---
+++ src/Nodo.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+class Nodo {
+    int id;
+    List<Arista> conexiones; //Cada nodo viene con sus pesos
+    Nodo padre;
+
+    public Nodo(int id) {
+        this.id = id;
+        this.conexiones = new ArrayList<>();
+        this.padre=null;
+    }
+
+    public void conectarCon(Nodo destino, int tiempo) {
+        this.conexiones.add(new Arista(destino, tiempo));
+        
+    }
+
+    public Nodo(int id, List<Arista> conexiones, Nodo padreaux) {
+        this.id = id;
+        this.conexiones = conexiones;
+        this.padre = padreaux;
+    }
+
+    @Override
+    public String toString() { return "Nodo " + id; }
+}//Nodo
